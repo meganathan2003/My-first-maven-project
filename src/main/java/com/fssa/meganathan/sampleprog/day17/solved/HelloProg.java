0ae2b0b7a888c31b6da06b38cf76f43abc9181d4
Meganathan <meganathan212003@gmail.com>
2023-06-23 18:38:05 +0530

    first commit

---
+++ src/main/java/com/fssa/meganathan/sampleprog/day17/solved/HelloProg.java
@@ -0,0 +1,12 @@
+package com.fssa.meganathan.sampleprog.day17.solved;
+
+public class HelloProg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello world");
+
+	}
+
+}
