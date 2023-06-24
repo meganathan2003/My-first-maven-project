5272d02be3638edd091ffa8ed0785f5a5b27f689
Meganathan <meganathan212003@gmail.com>
2023-06-24 13:30:31 +0530

    Alter the files

--- src/main/java/com/fssa/meganathan/sampleprog/day17/solved/HelloProg.java
+++ src/main/java/com/fssa/meganathan/sampleprog/day17/solved/HelloProg.java
@@ -5,7 +5,7 @@ public class HelloProg {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hello world");
+		System.out.println("Hello world"); // output the hello world
 
 	}
 
